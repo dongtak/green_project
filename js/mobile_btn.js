9dcc8f9c6fb37661f542e1e92d0f0dbe97df341c
dongtak <sourcefilmer@gmail.com>
2023-03-30 15:28:19 +0900

    버그 수정

--- js/mobile_btn.js
+++ js/mobile_btn.js
@@ -24,54 +24,31 @@
 //       });
 // };
 
-// function hamburger() {
-//   // 스크롤 이벤트 추가
-//   window.addEventListener("scroll", () => {
-//     let mobileBtn = document.querySelector(".mobile_btn");
-//     let header = document.querySelector("header");
-//     if (document.documentElement.scrollTop > 0 && !hamburger.checked) {
-//       mobileBtn.style.display = "none";
-//       header.style.display = "none";
-//     } else {
-//       mobileBtn.style.display = "block";
-//       header.style.display = "block";
-//     }
-//   });
-
-//   // 햄버거 버튼 클릭 이벤트 추가
-//   hamburger.addEventListener("change", () => {
-//     let mobileBtn = document.querySelector(".mobile_btn");
-//     let header = document.querySelector("header");
-//     if (hamburger.checked) {
-//       mobileBtn.style.display = "block";
-//       header.style.display = "block";
-//     } else {
-//       mobileBtn.style.display = "none";
-//       header.style.display = "none";
-//     }
-//   });
-// }
-function hamburger_btn(){
-  document.addEventListener('touchmove', function(e) {
-    let mobileBtn = document.querySelector('.mobile_btn');
-    if (e.touches[0].pageY > 0 && !hamburger.checked) {
-      mobileBtn.style.display = 'none';
-  
+function hamburger() {
+  // 스크롤 이벤트 추가
+  window.addEventListener("scroll", () => {
+    let mobileBtn = document.querySelector(".mobile_btn");
+    let header = document.querySelector("header");
+    if (document.documentElement.scrollTop > 0 && !hamburger.checked) {
+      mobileBtn.style.display = "none";
+      header.style.display = "none";
     } else {
-      mobileBtn.style.display = 'block';
+      mobileBtn.style.display = "block";
+      header.style.display = "block";
     }
   });
-  
-  hamburger.addEventListener('change', () => {
-    let mobileBtn = document.querySelector('.mobile_btn');
+
+  // 햄버거 버튼 클릭 이벤트 추가
+  hamburger.addEventListener("change", () => {
+    let mobileBtn = document.querySelector(".mobile_btn");
+    let header = document.querySelector("header");
     if (hamburger.checked) {
-      mobileBtn.style.display = 'block';
-    } else if (document.documentElement.pageYOffset === 0) {
-      mobileBtn.style.display = 'block';
-    }
-    else {
-      mobileBtn.style.display = 'none';
+      mobileBtn.style.display = "block";
+      header.style.display = "block";
+    } else {
+      mobileBtn.style.display = "none";
+      header.style.display = "none";
     }
   });
-  
 }
+
