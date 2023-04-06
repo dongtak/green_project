4a9d40cb48703d9dd5f07afab928cbd9e2ff84d2
dongtak <sourcefilmer@gmail.com>
2023-04-06 19:05:49 +0900

    간격, 헤더 로고추가

--- js/signin.js
+++ js/signin.js
@@ -1,28 +1,51 @@
-const form =  document.getElementById('form');
-const id = document.getElementById('id');
-const password = document.getElementById('password');
+// const form =  document.getElementById('form');
+// const id = document.getElementById('id');
+// const password = document.getElementById('password');
 
-form.addEventListener('summit', e => {
-   e.preventDefault(); /* 양식이 제출 되는 것을 방지하기 위해 이벤트에 pre-- 메서드를 호출. */
+// form.addEventListener('summit', e => {
+//    e.preventDefault(); /* 양식이 제출 되는 것을 방지하기 위해 이벤트에 pre-- 메서드를 호출. */
 
-   validateInputs();
-});
+//    validateInputs();
+// });
 
-function validateInputs() {
-    const idvalue = idvalue.value.trim();
-    const passwordValue = password.value.trim();
+// function validateInputs() {
+//     const idvalue = idvalue.value.trim();
+//     const passwordValue = password.value.trim();
 
-    if (idvalue === '') {
-        setError(id, 'id is required');
-    } else {
-        setSuccess(id);
+//     if (idvalue === '') {
+//         setError(id, 'id is required');
+//     } else {
+//         setSuccess(id);
+//     }
+
+//     if (passwordValue === '') {
+//         setError(password, 'password is required');
+//     } else if (passwordValue.length < 8) {
+//         setError(password, 'password must be at least 8 character.')
+//     } else {
+//         setSuccess(password);
+//     }
+// }
+// 로그인 유효성 체크
+function loginChk() {
+    var getCheck = RegExp(/^[a-zA-Z0-9]{4,12}$/);
+  
+    // 아이디 공백 확인
+    if ($("#uid").val() == "") {
+      alert("아이디를 입력해 주세요");
+      $("#uid").focus();
+      return false;
     }
-
-    if (passwordValue === '') {
-        setError(password, 'password is required');
-    } else if (passwordValue.length < 8) {
-        setError(password, 'password must be at least 8 character.')
-    } else {
-        setSuccess(password);
+  
+    // 비밀번호 유효성 검사
+    if (!getCheck.test($("#pwd").val())) {
+      alert("아이디 또는 비밀번호를 잘못 입력했습니다. 입력하신 내용을 다시 확인해주세요.");
+    //   $("#pwd").val("");
+    //   $("#pwd").focus();
+      return false;
     }
-}
+  
+    // 유효성 검사 통과
+    return true;
+  }
+  
--- js/signup.js
+++ js/signup.js
@@ -1,52 +1,77 @@
 
-   function validate() {
-       var re = /^[a-zA-Z0-9]{4,12}$/ // 아이디와 패스워드가 적합한지 검사할 정규식
-       var re2 = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
-       // 이메일이 적합한지 검사할 정규식
+//    function validate() {
+//        var re = /^[a-zA-Z0-9]{4,12}$/ // 아이디와 패스워드가 적합한지 검사할 정규식
+//        var re2 = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
+//        // 이메일이 적합한지 검사할 정규식
 
-       var id = document.getElementById("id");
-       var pw = document.getElementById("pw");
-       var email = document.getElementById("email");
+//        var id = document.getElementById("id");
+//        var pw = document.getElementById("pw");
+//        var email = document.getElementById("email");
 
 
-       if(!check(re,id,"아이디는 4~12자의 영문 대소문자와 숫자로만 입력")) {
-           return false;
-       }
+//        if(!check(re,id,"아이디는 4~12자의 영문 대소문자와 숫자로만 입력")) {
+//            return false;
+//        }
 
-       if(!check(re,pw,"패스워드는 4~12자의 영문 대소문자와 숫자로만 입력")) {
-           return false;
-       }
+//        if(!check(re,pw,"패스워드는 4~12자의 영문 대소문자와 숫자로만 입력")) {
+//            return false;
+//        }
 
-       if(join.pw.value != join.checkpw.value) {
-           alert("비밀번호가 다릅니다. 다시 확인해 주세요.");
-           join.checkpw.value = "";
-           join.checkpw.focus();
-           return false;
-       }
+//        if(join.pw.value != join.checkpw.value) {
+//            alert("비밀번호가 다릅니다. 다시 확인해 주세요.");
+//            join.checkpw.value = "";
+//            join.checkpw.focus();
+//            return false;
+//        }
 
-       if(email.value=="") {
-           alert("이메일을 입력해 주세요");
-           email.focus();
-           return false;
-       }
+//        if(email.value=="") {
+//            alert("이메일을 입력해 주세요");
+//            email.focus();
+//            return false;
+//        }
 
-       if(!check(re2, email, "적합하지 않은 이메일 형식입니다.")) {
-           return false;
-       }
+//        if(!check(re2, email, "적합하지 않은 이메일 형식입니다.")) {
+//            return false;
+//        }
 
-       if(join.name.value=="") {
-           alert("이름을 입력해 주세요");
-           join.name.focus();
-           return false;
-       }
+//        if(join.name.value=="") {
+//            alert("이름을 입력해 주세요");
+//            join.name.focus();
+//            return false;
+//        }
 
-   function check(re, what, message) {
-       if(re.test(what.value)) {
-           return true;
-       }
-       alert(message);
-       what.value = "";
-       what.focus();
-       //return false;
-   }
-  }
+//    function check(re, what, message) {
+//        if(re.test(what.value)) {
+//            return true;
+//        }
+//        alert(message);
+//        what.value = "";
+//        what.focus();
+//        //return false;
+//    }
+//   }
+// 로그인 유효성 체크
+function loginChk() {
+    var getCheck= RegExp(/^[a-zA-Z0-9]{4,12}$/);
+
+    //아이디 공백 확인
+    if($("#uid").val() == ""){
+    alert("아이디를 입력해 주세요");
+    $("#uid").focus();
+    return false;
+    }
+
+    // if($("#pwd").val() == ""){
+    //     alert("비밀번호를 입력해 주세요");
+    //     $("#pwd").focus();
+    //     return false;
+    // }
+
+    if(!getCheck.test($("#pwd").val())){
+        alert("아이디 또는 비밀번호를 잘못 입력했습니다.                                   입력하신 내용을 다시 확인해주세요.");
+        $("#pwd").val("");
+        $("#pwd").focus();
+        return false;
+    }
+
+}
